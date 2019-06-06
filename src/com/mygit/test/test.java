54745952e0c55a0a0eee44367cdde2cfa257953e
qi-bing.zuo <zuoqibing@xywy.com>
2019-06-06 15:49:47 +0800

    Initial commit

---
+++ src/com/mygit/test/test.java
@@ -0,0 +1,17 @@
+package com.mygit.test;
+
+/**
+ * @ProjectName: mygit
+ * @Package: com.mygit.test
+ * @ClassName: test
+ * @Author: qbzuo
+ * @Description:
+ * @Date: 2019/6/6 15:46
+ * @Version: 1.0
+ */
+public class test {
+
+     public static void main(String[] args){
+         System.out.println("2*2="+(2*2));
+     }
+}
